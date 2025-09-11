e26418b4c327f6907bea00dfb42cb1fb0a25f3ed
Kosala Tennakoon <kosala.me@gmail.com>
2025-09-11 22:58:05 +0530

    task: added comments

--- config/settings.go
+++ config/settings.go
@@ -1,5 +1,9 @@
 package config
 
+// Settings contain additional configuration values used by the config parser in order to parse the config structure
+// that is provided.
+// Under the hood the config parser uses 'github.com/spf13/viper' to parse configurations.
+// So the config struct 'MUST' contain `mapstructure` tags in order for parser to do the mapping.
 type Settings struct {
 	// Directory in which the configuration file is located in.
 	Dir string
@@ -11,7 +15,25 @@ type Settings struct {
 	// prevents you from using a system variable that you have not defined yourself.
 	Prefix string
 
-	// Default values to be used in case they are not provided
+	// Default values to be used in case they are not provided.
+	// Keys of the `Settings.Default` map should be dot (.) concatenated.
+	// For a config struct like this,
 	//
+	//	type Config struct {
+	//		App AppConfig `mapstructure:"app"`
+	//	}
+	//
+	//	type AppConfig struct {
+	//	 Name     string       `mapstructure:"name"`
+	//		Metrics  MetricConfig `mapstructure:"metrics"`
+	//	}
+	//
+	//	type MetricConfig struct {
+	//		Enabled bool   `mapstructure:"enabled"`
+	//		Port    int    `mapstructure:"port"`
+	//		Route   string `mapstructure:"route"`
+	//	}
+	//
+	// Use `app.metrics.enabled` as the key to set a default value for the metrics enabled configuration.
 	Defaults map[string]any
 }
